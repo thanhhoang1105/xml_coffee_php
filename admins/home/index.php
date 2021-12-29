018509f7ffeb4b61de550ebc276dcd6d07044880
thanhhoang1105 <thanhhoangngoc.bmt@gmail.com>
2021-12-29 16:58:23 +0700

    first commit

---
+++ admins/home/index.php
@@ -0,0 +1,218 @@
+<?php 
+    if(isset($_GET['action'])){
+        $cafe_shop = simplexml_load_file('../xml/cafe.xml');
+        $id = $_GET['id'];
+        $index =  0;
+        $i = 0 ;
+        foreach ($cafe_shop->packages as $packages){
+            if($packages['id']==$id){
+                $index = $i;
+                break;
+            }
+            $i++;
+        }
+        unset ($cafe_shop->packages[$index]);
+        file_put_contents('../xml/cafe.xml',$cafe_shop->asXML());
+    }
+    $cafe_shop = simplexml_load_file('../xml/cafe.xml');
+    $xml_url = 'http://localhost:8888/xml/Coffee/images/';
+        
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/favicon.ico" type="image/ico" />
+
+    <title>Gentelella Alela! | </title>
+
+    <!-- Bootstrap -->
+    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.0/css/all.min.css"
+        integrity="sha512-gRH0EcIcYBFkQTnbpO8k0WlsD20x5VzjhOA1Og8+ZUAhcMUCvd+APD35FJw3GzHAP3e+mP28YcDJxVr745loHw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.min.css" rel="stylesheet">
+
+    <!-- Style css -->
+    <link href="../build/css/styles.css" rel="stylesheet">
+
+</head>
+
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+                    <div class="navbar nav_title" style="border: 0;">
+                        <a href="../index.php" class="site_title"> <i class="fas fa-mug-hot"></i>
+                            <span>Coffee</span></a>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <!-- menu profile quick info -->
+                    <div class="profile clearfix">
+                        <div class="profile_pic">
+                            <img src="../images/ngocngu.jpg" alt="..." class="img-circle profile_img">
+                        </div>
+                        <div class="profile_info">
+                            <span>Welcome</span>
+                            <h2>Ngọc Ngu</h2>
+                        </div>
+                    </div>
+                    <!-- /menu profile quick info -->
+
+                    <br />
+
+                    <!-- sidebar menu -->
+                    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                        <div class="menu_section">
+                            <h3>Quản lý</h3>
+                            <ul class="nav side-menu">
+                                <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu">
+                                        <li><a href="../home/index.php">Header</a></li>
+                                        <li><a href="backgroundVD">Video Background Home</a></li>
+                                    </ul>
+                                </li>
+                                <li><a><i class="fa fa-edit"></i> Section<span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu">
+                                        <li><a href="package">Package</a></li>
+                                        <li><a href="service">Service</a></li>
+                                        <li><a href="form_validation.html">Form Validation</a></li>
+                                        <li><a href="form_wizards.html">Form Wizard</a></li>
+                                        <li><a href="form_upload.html">Form Upload</a></li>
+                                        <li><a href="form_buttons.html">Form Buttons</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /sidebar menu -->
+
+                    <!-- /menu footer buttons -->
+                    <div class="sidebar-footer hidden-small">
+                        <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+                            <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+                        </a>
+                    </div>
+                    <!-- /menu footer buttons -->
+                </div>
+            </div>
+
+            <!-- top navigation -->
+            <div class="top_nav">
+                <div class="nav_menu">
+                    <div class="nav toggle">
+                        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                    </div>
+                    <nav class="nav navbar-nav">
+                        <ul class=" navbar-right">
+                            <li class="nav-item dropdown open" style="padding-left: 15px;">
+                                <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true"
+                                    id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+                                    <img src="../images/ngocngu.jpg" alt="">Ngọc Ngu
+                                </a>
+                                <div class="dropdown-menu dropdown-usermenu pull-right"
+                                    aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="login.html"><i class="fa fa-sign-out pull-right"></i>
+                                        Log Out</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <!-- /top navigation -->
+
+            <!-- page content -->
+            <div class="right_col" role="main">
+
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 ">
+
+                        <div class="row x_title">
+                            <div class="col-md-6">
+                                <h2>
+                                    <a href="add.php">Add Header <i class="fas fa-plus"></i></a>
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="dashboard_graph">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>image</th>
+                                        <th>numeric</th>
+                                        <th>name</th>
+                                        <th class="des">description</th>
+                                        <th>button</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($cafe_shop->packages as $packages) { ?>
+                                    <tr>
+                                        <td><?php echo $packages['id'] ; ?></td>
+                                        <td><?php echo '<img src="'.$xml_url . '' . $packages -> image . '" class="img_with">' ?>
+                                        </td>
+                                        <td><?php echo $packages->numeric ; ?></td>
+                                        <td><?php echo $packages->name ; ?></td>
+                                        <td><?php echo $packages->description ; ?></td>
+                                        <td><?php echo $packages->button ; ?></td>
+                                        <td>
+                                            <a class="edit" href="edit.php?id=<?php echo $packages['id'];?>"><i
+                                                    class="fas fa-pen"></i></a>
+                                            <a class="delete"
+                                                href="index.php?action=delete&id=<?php echo $packages['id'];?>"
+                                                onclick="return confirm ('Are you sure?')"><i
+                                                    class="fas fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <br />
+        </div>
+        <!-- /page content -->
+    </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+
+    <script src="../vendors/Flot/jquery.flot.js"></script>
+    <script src="../vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="../vendors/Flot/jquery.flot.time.js"></script>
+    <script src="../vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="../vendors/Flot/jquery.flot.resize.js"></script>
+
+    <script src="../vendors/DateJS/build/date.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+
+</body>
+
+</html>
